2cf2cec928cbcefc7fbf058abc3ab7ccf86c5b4b
285672259@qq.com <a6470280>
2017-04-10 15:02:46 +0800

    11

--- dist/FrontEnd/server/js/404.js
+++
@@ -1,7 +0,0 @@
-module.exports = async (ctx, next) => {
-    if (ctx.status === 404 && ctx.req.method === "GET") {
-        await ctx.redirect("/404/");
-    } else {
-        next();
-    }
-};
--- dist/FrontEnd/server/js/response.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    success: (ctx, message = "") => {
-        ctx.body = {success: true, message: message};
-    },
-    fail: (ctx, message = "") => {
-        ctx.body = {success: false, message: message};
-    }
-};
